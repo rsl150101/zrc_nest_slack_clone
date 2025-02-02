cf9516e517fccdde34ef724b0fe79d20feba38f8
rsl150101 <dnwnd501@naver.com>
2025-02-02 22:23:05 +0900

    [Add] 2-5 custom decorator: User, Token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 import { ApiOkResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserDto } from 'src/common/dto/user.dto';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('api/users')
 export class UsersController {
@@ -13,8 +14,8 @@ export class UsersController {
   })
   @ApiOperation({ summary: '내 정보 조회' })
   @Get()
-  getUsers(@Req() req) {
-    return req.user;
+  getUsers(@User() user) {
+    return user;
   }
 
   @ApiOperation({ summary: '회원가입' })
@@ -32,8 +33,8 @@ export class UsersController {
   })
   @ApiOperation({ summary: '로그인' })
   @Post('login')
-  login(@Req() req) {
-    return req.user;
+  login(@User() user) {
+    return user;
   }
 
   @ApiOperation({ summary: '로그아웃' })
