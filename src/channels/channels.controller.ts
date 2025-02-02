549b805a020e367acc72dd702af553224203a2c9
rsl150101 <dnwnd501@naver.com>
2025-02-02 19:26:13 +0900

    [Add] 2-3 channels and workspaces API

---
+++ src/channels/channels.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+@Controller('api/workspaces/:url/channels')
+export class ChannelsController {
+  @Get()
+  getAllChannels() {}
+
+  @Post()
+  createChannels() {}
+
+  @Get(':name')
+  getSpecificChannel() {}
+
+  @Get(':name/chats')
+  getChats(@Query() query, @Param() param) {
+    console.log(query.perPage, query.page);
+    console.log(param.id, param.url);
+  }
+
+  @Post(':name/chats')
+  postChat() {}
+
+  @Get(':name/members')
+  getAllMembers() {}
+
+  @Post(':name/members')
+  inviteMembers() {}
+}
